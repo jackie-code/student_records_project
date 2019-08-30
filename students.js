0d4e8f4afda3e06151188644704d24088fd3a745
jackie-code <paradiseprofessional88@gmail.com>
2019-08-30 21:40:19 +0800

    initial commit

---
+++ students.js
@@ -0,0 +1,20 @@
+var students = [
+    {
+        name: 'Dave',
+        track: 'Front End Developer',
+        achievements: 158,
+        points: 14730
+    },
+    {
+        name: 'Jody',
+        track: 'IOS Development with Swift',
+        achievements: 175,
+        points: 16375,
+    },
+    {
+        name: 'John',
+        track: 'Learn WordPress',
+        achievements: 40,
+        points: 5000
+    }
+];
